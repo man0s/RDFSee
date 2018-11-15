24700213c6b7822548ad30ee52b54b7a44a59845
Emmanouil Katefidis <katefidis@ceid.upatras.gr>
2018-11-15 18:20:31 +0200

    Update JavaFX template

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,9 +12,8 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 600, 450));
         primaryStage.show();
-        System.out.println("test");
     }
 
 
