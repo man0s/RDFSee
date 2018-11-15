8951c7caf4062e4b279a8d06560acfa8f4b4171a
Emmanouil Katefidis <katefidis@ceid.upatras.gr>
2018-11-15 15:56:22 +0200

    Update source code

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -14,6 +14,7 @@ public class Main extends Application {
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
+        System.out.println("test");
     }
 
 
