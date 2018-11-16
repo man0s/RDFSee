f2c0a7e91be3b6e22a2738303967cd1cf043c7b5
Emmanouil Katefidis <katefidis@ceid.upatras.gr>
2018-11-16 20:22:48 +0200

    Update controller

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,50 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
 public class Controller {
+    @FXML
+    public Button rdfButton;
+    @FXML
+    public Button professorButton;
+    @FXML
+    public Button studentButton;
+    @FXML
+    public Button departmentButton;
+    @FXML
+    public Button lessonButton;
+    @FXML
+    public Button aboutButton;
+    @FXML
+    public TableView dataTable;
+    @FXML
+    private TableColumn nameColumn;
+    @FXML
+    private TableColumn ageColumn;
+    @FXML
+    private TableColumn phoneColumn;
+    @FXML
+    public ChoiceBox filterChoice;
+    @FXML
+    public TextField searchQuery;
+    @FXML
+    public Button searchButton;
+    @FXML
+    public ChoiceBox departmentChoice;
+    @FXML
+    public TableView triplesTable;
+    @FXML
+    private TableColumn subjectColumn;
+    @FXML
+    private TableColumn predicateColumn;
+    @FXML
+    private TableColumn objectColumn;
+    @FXML
+    public TextField uriQuery;
+    @FXML
+    public Button uriButton;
+
+
+
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,7 +4,10 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+
+import java.io.File;
 
 
 public class Main extends Application {
@@ -12,12 +15,22 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("RDFSee");
         primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
 
+        final FileChooser fileChooser = new FileChooser();
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("RDF", "*.rdf"));
+
     }
 
+
+    private static void configureFileChooser(final FileChooser fileChooser){
+        fileChooser.setTitle("View Files");
+        fileChooser.setInitialDirectory(
+                new File(System.getProperty("user.home") + "/Desktop")
+        );
+    }
 
     public static void main(String[] args) {
         launch(args);
