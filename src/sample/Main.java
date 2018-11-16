91e27e30b490062434abbe47e5d6e5d5f659dae3
Emmanouil Katefidis <katefidis@ceid.upatras.gr>
2018-11-16 17:55:37 +0200

    Update fxml

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 600, 450));
+        primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
     }
 
