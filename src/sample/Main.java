21fb7ae967ef0e1a8ab404dc510ce2bf2cfac38a
Emmanouil Katefidis <katefidis@ceid.upatras.gr>
2018-11-16 19:56:23 +0200

    Update FXML

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,6 +6,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+
 public class Main extends Application {
 
     @Override
@@ -14,6 +15,7 @@ public class Main extends Application {
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
+
     }
 
 
