d082d54eaa83ee7da133bb9b736b9dd5e81727af
Emmanouil Katefidis <katefidis@ceid.upatras.gr>
2018-11-16 21:59:34 +0200

    Update Main.java

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,21 +1,25 @@
 package sample;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 public class Controller {
     @FXML
-    public Button rdfButton;
+    public MenuItem rdfButton;
     @FXML
-    public Button professorButton;
+    public MenuItem professorButton;
     @FXML
-    public Button studentButton;
+    public MenuItem studentButton;
     @FXML
-    public Button departmentButton;
+    public MenuItem departmentButton;
     @FXML
-    public Button lessonButton;
+    public MenuItem lessonButton;
     @FXML
-    public Button aboutButton;
+    public MenuItem aboutButton;
     @FXML
     public TableView dataTable;
     @FXML
@@ -44,6 +48,70 @@ public class Controller {
     public TextField uriQuery;
     @FXML
     public Button uriButton;
+
+    private final ObservableList<Data> data = FXCollections.observableArrayList();
+    private final ObservableList<URIData> uridata = FXCollections.observableArrayList();
+
+    public void setData(String name, String age, String phone) {
+        data.add(new Data(name, age, phone));
+        nameColumn.setCellValueFactory(new PropertyValueFactory<Data,String>("Name"));
+        ageColumn.setCellValueFactory(new PropertyValueFactory<Data,String>("Age"));
+        phoneColumn.setCellValueFactory(new PropertyValueFactory<Data,String>("Phone"));
+        nameColumn.setStyle("-fx-alignment: CENTER;");
+        ageColumn.setStyle("-fx-alignment: CENTER;");
+        phoneColumn.setStyle("-fx-alignment: CENTER;");
+        dataTable.setItems(data);
+    }
+
+    public void setUriData(String subject, String predicate, String object) {
+        uridata.add(new URIData(subject, predicate, object));
+        subjectColumn.setCellValueFactory(new PropertyValueFactory<URIData,String>("Subject"));
+        predicateColumn.setCellValueFactory(new PropertyValueFactory<URIData,String>("Predicate"));
+        objectColumn.setCellValueFactory(new PropertyValueFactory<URIData,String>("Object"));
+        subjectColumn.setStyle("-fx-alignment: CENTER;");
+        predicateColumn.setStyle("-fx-alignment: CENTER;");
+        objectColumn.setStyle("-fx-alignment: CENTER;");
+        triplesTable.setItems(uridata);
+    }
+
+
+    public static class Data {
+        private final SimpleStringProperty Name;
+        private final SimpleStringProperty Age;
+        private final SimpleStringProperty Phone;
+
+        private Data(String name, String age, String phone) {
+            this.Name = new SimpleStringProperty(name);
+            this.Age = new SimpleStringProperty(age);
+            this.Phone = new SimpleStringProperty(phone);
+        }
+
+        public String getName() { return Name.get(); }
+        public void setName(String name) { Name.set(name); }
+        public String getAge() { return Age.get(); }
+        public void setAge(String age) { Age.set(age); }
+        public String getPhone() { return Phone.get(); }
+        public void setPhone(String phone) { Phone.set(phone); }
+    }
+
+    public static class URIData {
+        private final SimpleStringProperty Subject;
+        private final SimpleStringProperty Predicate;
+        private final SimpleStringProperty Object;
+
+        private URIData(String subject, String predicate, String object) {
+            this.Subject = new SimpleStringProperty(subject);
+            this.Predicate = new SimpleStringProperty(predicate);
+            this.Object = new SimpleStringProperty(object);
+        }
+
+        public String getSubject() { return Subject.get(); }
+        public void setSubject(String subject) { Subject.set(subject); }
+        public String getPredicate() { return Predicate.get(); }
+        public void setPredicate(String predicate) { Predicate.set(predicate); }
+        public String getObject() { return Object.get(); }
+        public void setObject(String object) { Object.set(object); }
+    }
 
 
 
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,24 +4,57 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import org.apache.jena.rdf.model.*;
+import org.apache.jena.util.FileManager;
 
 import java.io.File;
+import java.io.InputStream;
 
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("RDFSee");
-        primaryStage.setScene(new Scene(root, 800, 600));
-        primaryStage.show();
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("sample.fxml"));
+        Parent root = loader.load();
+        Controller controller = loader.getController();
 
         final FileChooser fileChooser = new FileChooser();
         fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("RDF", "*.rdf"));
 
+        controller.rdfButton.setOnAction((event) -> {
+            configureFileChooser(fileChooser);
+            File file = fileChooser.showOpenDialog(primaryStage);
+            if (file != null) {
+                try {
+                    // create an empty model
+                    Model model = ModelFactory.createDefaultModel();
+
+
+                } catch (Exception ex) {
+                    Alert alert = new Alert(Alert.AlertType.ERROR);
+                    alert.setTitle("File Parsing Error");
+                    alert.setHeaderText("Something went wrong with the parsing..!");
+                    alert.showAndWait();
+                    ex.printStackTrace();
+                }
+            } else {
+                Alert alert = new Alert(Alert.AlertType.WARNING);
+                alert.setTitle("File Selection Error");
+                alert.setHeaderText("Please select a valid RDF file..!");
+                alert.showAndWait();
+            }
+        });
+
+        controller.setData("Emmanouil Katefidis", "21", "2610621332");
+        controller.setUriData("Panagiota Preza", "22", "2610987883");
+
+        primaryStage.setTitle("RDFSee");
+        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.show();
     }
 
 
